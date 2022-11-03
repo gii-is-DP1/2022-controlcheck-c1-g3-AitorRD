9159b793e1e28fadda5abe195bc20079b205607b
AitorRD <aitroddue@alum.us.es>
2022-11-03 17:21:16 +0100

    Entrega control check

--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomRepository.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomRepository.java
@@ -3,10 +3,17 @@ package org.springframework.samples.petclinic.recoveryroom;
 import java.util.List;
 import java.util.Optional;
 
-public interface RecoveryRoomRepository {
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecoveryRoomRepository extends CrudRepository<RecoveryRoom, Integer>{
     List<RecoveryRoom> findAll();
     Optional<RecoveryRoom> findById(int id);
     RecoveryRoom save(RecoveryRoom p);
-    //List<RecoveryRoomType> findAllRecoveryRoomTypes();
+
+    @Query("SELECT rtype from RecoveryRoomType rtype")
+    List<RecoveryRoomType> findAllRecoveryRoomTypes();
     //RecoveryRoomType getRecoveryRoomType(String name);
 }
--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomType.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomType.java
@@ -1,11 +1,33 @@
 package org.springframework.samples.petclinic.recoveryroom;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Length;
+import org.springframework.samples.petclinic.model.BaseEntity;
+import org.springframework.samples.petclinic.model.NamedEntity;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
+@Table(name="recovery_room_type")
 public class RecoveryRoomType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
+
+    @NotBlank
+    @Size(min = 3, max = 50)
     String name;
 }
